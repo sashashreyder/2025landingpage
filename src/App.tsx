a5ea7b95aac352da11e970425823d87148d834e2
sashashreyder <sashashreiderbr@gmail.com>
2025-08-08 16:05:18 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <p>Your clean React + TypeScript + Vite project is ready!</p>
+    </div>
+  )
+}
+
+export default App
